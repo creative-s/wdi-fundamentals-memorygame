eb4b6adbde30700dd7155e74bfed2273573ab222
Stephanie Alvarado <creatives@Stephanies-MBP.home>
2019-06-16 13:19:00 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //
 var cards = [
 {
+	cardImage: "images/back.png"
+},
+{
 	rank: "queen",
 	suit:"hearts",
 	cardImage: "images/queen-of-hearts.png"
@@ -28,6 +31,7 @@ var cardsInPlay = [];
 
 function checkForMatch(){
 
+this.setAttribute(cards[cardId].cardImage);
 if (cardsInPlay[0] === cardsInPlay[1]) {
  	alert("You found a match!");
 }else {
@@ -35,8 +39,9 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 	}
 };
 
-function flipCard(cardId){
+function flipCard(){
 
+var cardId = this.getAttribute('data-id');
 console.log("User flipped" + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
@@ -47,5 +52,27 @@ checkForMatch();
 
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('img');
+		cardElement = document.createElement('img');
+		cardElement = document.createElement('img');
+		cardElement = document.createElement('img');
+		cardElement.setAttribute('src' , 'images/back.png');
+		cardElement.setAttribute('data-id' , i);
+		cardElement.addEventListener('click', flipCard);
+
+
+document.getElementById('board-game').appendChild(cardElement);
+
+
+	}
+};
+
+createBoard(0,1);
+
+
+
+
+
